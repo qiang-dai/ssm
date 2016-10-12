8d1428c6bfaf5770cb3daedb87f34ca6e24f868a
后端组-戴强 <qiang.dai@xinmei365.com>
2016-10-12 16:25:37 +0800

    取消人工gc

--- src/main/java/com/cn/hnust/service/impl/sticker/ThirdPartyStickerServiceImpl.java
+++ src/main/java/com/cn/hnust/service/impl/sticker/ThirdPartyStickerServiceImpl.java
@@ -122,9 +122,9 @@ public class ThirdPartyStickerServiceImpl implements IThirdPartyStickerInfoServi
                 System.out.println("getDocBadCnt= " + getDocBadCnt);
                 System.out.println("successCnt= " + successCnt);
 
-                if (i % 100 == 0) {
-                    System.gc();
-                }
+//                if (i % 100 == 0) {
+//                    System.gc();
+//                }
             } catch (Exception e) {
                 System.out.println("error!");
             }
